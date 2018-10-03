f4abb0624dada0eb3a75531fc482934cc2e0b85c
Megan Farley <megfarley4@gmail.com>
2018-10-03 20:06:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
-  phrase = "The line is currently: "
+  phrase = "The line is currently:"
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |deli, index|
-      phrase += "#{index + 1}. #{deli} "
+      phrase += " #{index + 1}. #{deli}"
     end
     puts phrase
   end
