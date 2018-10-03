789c33ab0b7b68a5b1667ce5981b95a4ddc4876f
Megan Farley <megfarley4@gmail.com>
2018-10-03 20:22:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,4 +18,6 @@ def take_a_number(katz_deli, name)
 end
 def now_serving(katz_deli)
   if katz_deli.size == 0
-    puts "There is nobody waiting to be served"
+    puts "There is nobody waiting to be served."
+  else
+    
