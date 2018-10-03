734bb808e9a64487a336c04a777c071ecd1bde6c
Megan Farley <megfarley4@gmail.com>
2018-10-03 20:05:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |deli, index|
+      phrase += "#{index + 1}. #{deli} "
+    end
+    puts phrase
   end
 end
