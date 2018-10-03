1d62975f85c9edb0075cc3c28ba655aac2c37c01
Megan Farley <megfarley4@gmail.com>
2018-10-03 20:00:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
+  phrase = "The line is currently: "
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    katz_deli.size + 1
+    
   end
 end
